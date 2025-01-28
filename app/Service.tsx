9f4daf5f043e54cb15f8b8a4fe732ad8931cacf6
chowan kumar <chowanmalhani5@gmail.com>
2025-01-28 12:20:55 +0500

    add servives

--- app/Service.tsx
+++ app/Service.tsx
@@ -6,6 +6,9 @@ import { GiBurningRoundShot } from "react-icons/gi";
 import { RiGlobalLine } from "react-icons/ri";
 import { GiBreakingChain } from "react-icons/gi";
 import { IoMdLaptop } from "react-icons/io";
+import { FaCloud } from "react-icons/fa6";
+import { FaMobile } from "react-icons/fa";
+import { GiCyberEye } from "react-icons/gi";
 
 const cardData = [
   {
@@ -23,6 +26,22 @@ const cardData = [
     title: "Digital Marketing",
     description: "Boosting your online presence with data-driven digital marketing strategies for maximum reach and impact.",
   },
+  {
+    icon: <FaCloud className="inline-block mr-2" />,
+    title: "Cloud Servive",
+    description: "Scalable and secure cloud solutions to streamline your business operations and enhance flexibility.",
+  },
+  
+  {
+    icon: <FaMobile className="inline-block mr-2" />,
+    title: "Mobile Application",
+    description: "Custom mobile app development to deliver user-friendly and innovative solutions for iOS and Android platforms.",
+  },
+  {
+    icon: <GiCyberEye className="inline-block mr-2" />,
+    title: "Cyber Security",
+    description: "Advanced cybersecurity services to protect your business from threats and ensure data integrity",
+  },
 ];
 
 
@@ -31,9 +50,10 @@ const Service = () => {
   return (
 
     <section className='mt-20 flex flex-col gap-10  dark:bg-surface-color bg-on-surface-color text-on-surface-color '>
+
       <h2 className="text-4xl font-bold text-on-primary dark:text-white text-center">Our Services</h2>
       {/* Cards */}
-      <div className="flex flex-col md:flex-row  gap-4 p-4">
+      <div className="grid grid-cols-1 lg:grid-cols-3 xl:grid-cols-4  gap-4 p-4">
         {cardData.map((card, index) => (
           <div
             key={index}
@@ -42,7 +62,7 @@ const Service = () => {
             <h1 className=" text-5xl font-semibold mb-2 text-secondary dark:text-primary">
               {card.icon}
             </h1>
-            <p className="dark:text-on-surface-color  text-black font-medium mb-5 mobile-headline-medium">{card.title}</p>
+            <p className="dark:text-on-surface-color  text-black font-medium mb-2 mobile-headline-medium">{card.title}</p>
             <p className="dark:text-on-surface-color  text-gray-600">{card.description}</p>
           </div>
         ))}
